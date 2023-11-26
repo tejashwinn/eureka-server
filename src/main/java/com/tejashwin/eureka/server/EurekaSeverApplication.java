de3820fcf5e639605ede848b2f33bdcbd7993799
tejashwin.u <tejashwin5104@gmail.com>
2023-11-26 12:31:26 +0530

    Init

---
+++ src/main/java/com/tejashwin/eureka/server/EurekaSeverApplication.java
@@ -0,0 +1,13 @@
+package com.tejashwin.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSeverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tejashwin/eureka/server/EurekaSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejashwin.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
