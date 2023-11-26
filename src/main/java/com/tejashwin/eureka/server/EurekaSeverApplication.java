72c2053ab127c4402496052e40dd9aee5e063b40
tejashwin.u <tejashwin5104@gmail.com>
2023-11-26 12:38:26 +0530

    Added Eureka Server configuration

--- src/main/java/com/tejashwin/eureka/server/EurekaSeverApplication.java
+++ src/main/java/com/tejashwin/eureka/server/EurekaSeverApplication.java
@@ -2,12 +2,14 @@ package com.tejashwin.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaSeverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaSeverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaSeverApplication.class, args);
+    }
 
 }
